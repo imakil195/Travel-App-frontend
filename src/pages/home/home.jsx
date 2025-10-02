14ccac7767219810b18ad1dfd86fdbcc69636267
“Akil <akilsaran195@gmail.com>
2025-10-02 11:02:23 +0530

    added features:navbar, category list , hotellisting page

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,64 @@
+import { Fragment, useState, useEffect } from "react";
+import { Navbar, HotelCard , Categories } from "../../components";
+import "./Home.css";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useCategory } from "../../context";
+export const Home = () => {
+  const [hasMore, setHasMore] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(16);
+  const [testData, setTestData] = useState([]);
+  const [hotels, setHotels] = useState([]);
+  const {hotelCategory} = useCategory()
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+      `https://travelstay-backendapp.onrender.com/api/hotels?category=${hotelCategory} `        );
+        setTestData(data);
+        setHotels(data ? data.slice(0, 16) : []);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [hotelCategory]);
+
+  const fetchMoreData = () => {
+    if (hotels.length >= testData.length) {
+      setHasMore(false);
+      return;
+    }
+
+    setTimeout(() => {
+      const nextIndex = currentIndex + 16;
+      setHotels((prev) => [
+        ...prev,
+        ...testData.slice(currentIndex, nextIndex),
+      ]);
+      setCurrentIndex(nextIndex);
+    }, 1000);
+  };
+
+  return (
+    <Fragment>
+      <Navbar />
+      <Categories/>
+      {hotels && hotels.length > 0 ? (
+        <InfiniteScroll
+          dataLength={hotels.length} 
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h3 className="alert-text">Loading...</h3>}
+          endMessage={<p className="alert-text">You have seen it all</p>} 
+        >
+          <main className="main d-flex align-center wrap gap-larger">
+            {hotels.map((hotel) => (
+              <HotelCard key={hotel._id} hotel={hotel} />
+            ))}
+          </main>
+        </InfiniteScroll>
+      ) : null}
+    </Fragment>
+  );
+};
